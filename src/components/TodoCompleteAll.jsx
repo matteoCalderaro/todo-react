3b64fa2d5cb8210944876f7c13b00b01fba0d6d3
Matteo Calderaro <matteocalderaro@gmail.com>
2022-03-17 09:55:52 +0100

    Episode 10 - useContext for State Management

--- src/components/TodoCompleteAll.jsx
+++ src/components/TodoCompleteAll.jsx
@@ -1,14 +1,21 @@
-import React from 'react';
-import PropTypes from 'prop-types';
+import React, { useContext } from 'react';
+import { TodosContext } from '../context/TodosContext';
 
-TodoCompleteAll.propTypes = {
-  completeAllTodos: PropTypes.func.isRequired,
-};
+function TodoCompleteAll() {
+  const { todos, setTodos } = useContext(TodosContext);
 
-function TodoCompleteAll(props) {
+  function completeAllTodos() {
+    const updatedTodos = todos.map(todo => {
+      todo.isComplete = true;
+      return todo;
+    });
+
+    setTodos(updatedTodos);
+  }
+
   return (
     <div>
-      <div onClick={props.completeAllTodos} className="button">
+      <div onClick={completeAllTodos} className="button">
         Check All
       </div>
     </div>
